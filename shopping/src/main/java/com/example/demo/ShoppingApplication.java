30a9eef089f4d08c7d68865e83ad90c15115bd97
shogo-3345 <s276174@wakayama-u.ac.jp>
2025-02-03 23:31:47 +0900

    mm

--- shopping/src/main/java/com/example/demo/ShoppingApplication.java
+++ shopping/src/main/java/com/example/demo/ShoppingApplication.java
@@ -3,13 +3,20 @@ package com.example.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("com.example.demo.repository")
-public class ShoppingApplication {
+public class ShoppingApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShoppingApplication.class, args);
 	}
+	
+	@Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	      return builder.sources(ShoppingApplication.class);
+	    }
 
 }
