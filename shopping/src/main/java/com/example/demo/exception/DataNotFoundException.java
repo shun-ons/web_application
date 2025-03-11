63b0a489dcd779516ef39a8fc4b9ad48c007861a
shun-ons <shun.ons1507@gmail.com>
2025-03-12 00:51:04 +0900

    add docComment to exeption

--- shopping/src/main/java/com/example/demo/exception/DataNotFoundException.java
+++ shopping/src/main/java/com/example/demo/exception/DataNotFoundException.java
@@ -1,5 +1,8 @@
 package com.example.demo.exception;
-
+/**
+ * データが見つからなかった場合の例外を扱うクラス.
+ * @author 夏木翔吾
+ */
 @SuppressWarnings("serial")
 public class DataNotFoundException extends RuntimeException {
     public DataNotFoundException(String msg) {
--- shopping/src/main/java/com/example/demo/exception/StockShortageException.java
+++ shopping/src/main/java/com/example/demo/exception/StockShortageException.java
@@ -1,5 +1,8 @@
 package com.example.demo.exception;
-
+/**
+ * 在庫がない場合の例外を扱うクラス.
+ * @author 夏木翔吾
+ */
 @SuppressWarnings("serial")
 public class StockShortageException extends RuntimeException {
     public StockShortageException(String msg) {
